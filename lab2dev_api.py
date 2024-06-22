12d091e632b56c8c2685f51572538c68617a03d0
Angelo Evangelista <angeloevan.ane@gmail.com>
2024-06-21 22:55:31 -0300

    feat: add user and password authentication support

--- lab2dev_api.py
+++ lab2dev_api.py
@@ -12,6 +12,28 @@ class Lab2DevApi:
   _http_client: requests.Session
 
   def __init__(self):
+    response = self.__get_cookie_response_from_user_and_password()
+
+    self._http_client = requests.Session()
+    self._http_client.cookies.update(response.cookies)
+
+    utils.print_json(self.get("collaborators/whoami"))
+
+  def __get_cookie_response_from_user_and_password(self):
+    response = requests.post(
+      "https://portal-api.lab2dev.com/auth/login/password",
+      json={
+        "email": os.environ['LAB2DEV_USER_EMAIL'],
+        "password": os.environ['LAB2DEV_USER_PASSWORD'],
+      },
+      headers={
+        "Origin": "https://apontamentos.lab2dev.com",
+      }
+    )
+
+    return response
+
+  def __get_cookie_response_from_web_app(self):
     portal_otp = browser_handler.get_portal_otp()
 
     print(f"OTP: {portal_otp}")
@@ -24,10 +46,7 @@ class Lab2DevApi:
       },
     )
 
-    self._http_client = requests.Session()
-    self._http_client.cookies.update(response.cookies)
-
-    utils.print_json(self.get("collaborators/whoami"))
+    return response
 
   def get(self, endpoint):
     url = f'{self._base_url}/{endpoint}'
